5fbcc1a7390db2cb186b952154605e23ad60f806
Epeldex <alexander.epelde@gmail.com>
2024-02-28 13:34:52 +0100

    primer commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("btnInicio").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+document.getElementById("btnQuienesSomos").addEventListener("click", function() {
+    window.location.href = "quienes_somos.html";
+});
+
+document.getElementById("btnNuestroCliente").addEventListener("click", function() {
+    window.location.href = "nuestro_cliente.html";
+});
+
+document.getElementById("btnNecesidadesCliente").addEventListener("click", function() {
+    window.location.href = "necesidades_cliente.html";
+});
+
+document.getElementById("btnInvestigacion").addEventListener("click", function() {
+    window.location.href = "investigacion.html";
+});
+
