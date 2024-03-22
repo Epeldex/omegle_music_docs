1ab4f584e2405e14778182e50fe22870fb00786b
Epeldex <alexander.epelde@gmail.com>
2024-03-22 12:10:03 +0100

    movidas

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
-document.getElementById("btnInicio").addEventListener("click", function() {
-    window.location.href = "index.html";
-});
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('.menu');
+        header.classList.toggle('scrolled', window.scrollY > 0);
+    });
 
-document.getElementById("btnQuienesSomos").addEventListener("click", function() {
-    window.location.href = "quienes_somos.html";
+    // Configura el botón de Omegle Music para que no haga nada al hacer clic
+    const omegleMusicBtn = document.getElementById('omegleMusicBtn');
+    if (omegleMusicBtn) {
+        omegleMusicBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+        });
+    }
 });
+/* JavaScript to toggle classes based on scroll position */
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('.menu');
+    const omegleMusicH1 = document.getElementById('omegleMusicBtn'); // Ensure this is the correct ID
+    const omegleMusicLink = document.getElementById('omegleMusicLink'); // Ensure this is the correct ID
 
-document.getElementById("btnNuestroCliente").addEventListener("click", function() {
-    window.location.href = "nuestro_cliente.html";
-});
-
-document.getElementById("btnNecesidadesCliente").addEventListener("click", function() {
-    window.location.href = "necesidades_cliente.html";
-});
-
-document.getElementById("btnInvestigacion").addEventListener("click", function() {
-    window.location.href = "investigacion.html";
+    window.addEventListener('scroll', function() {
+        // Check if the page has been scrolled down
+        if (window.scrollY > 0) {
+            header.classList.add('scrolled');
+            omegleMusicH1.style.display = 'block';
+            omegleMusicLink.style.display = 'none';
+        } else {
+            header.classList.remove('scrolled');
+            omegleMusicH1.style.display = 'none';
+            omegleMusicLink.style.display = 'block';
+        }
+    });
 });
 
